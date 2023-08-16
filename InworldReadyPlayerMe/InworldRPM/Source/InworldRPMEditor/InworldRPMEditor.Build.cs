b5e456a98acc76e29c0ece63f5888971e387e6c6
Matt Carey <MattCarey@gmx.com>
2023-08-16 09:25:03 -0400

    Remove Metahuman/RPM dependence on NDK

--- InworldReadyPlayerMe/InworldRPM/Source/InworldRPMEditor/InworldRPMEditor.Build.cs
+++ InworldReadyPlayerMe/InworldRPM/Source/InworldRPMEditor/InworldRPMEditor.Build.cs
@@ -24,7 +24,6 @@ public class InworldRPMEditor : ModuleRules
                 "InworldAIEditor",
                 "InworldAIClient",
                 "InworldAIIntegration",
-                "InworldAINdk",
 				"CoreUObject",
 				"Engine",
 			}
