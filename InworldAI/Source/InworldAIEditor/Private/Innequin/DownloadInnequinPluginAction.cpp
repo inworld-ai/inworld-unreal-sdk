6b7f3410b9afeecf307960be75f5d41263fa040d
Matt Carey <mcarey@inworld.ai>
2023-08-28 10:37:22 -0400

    Innequin plugin [CORE-2949] (#16)
    [CORE-2949] T3 Support for Innequin
    
    Add functionality for downloading Innequin content plugin
    Add T3 innequin creator, similar to RPM

---
+++ InworldAI/Source/InworldAIEditor/Private/Innequin/DownloadInnequinPluginAction.cpp
@@ -0,0 +1,141 @@
+/**
+ * Copyright 2022 Theai, Inc. (DBA Inworld)
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ */
+
+
+#include "DownloadInnequinPluginAction.h"
+
+#include "InworldEditorApi.h"
+#include "Developer/DesktopPlatform/Public/IDesktopPlatform.h"
+#include "Developer/DesktopPlatform/Public/DesktopPlatformModule.h"
+#include "Misc/MonitoredProcess.h"
+#include "Async/Async.h"
+#include "Async/TaskGraphInterfaces.h"
+
+UDownloadInnequinPluginAction* UDownloadInnequinPluginAction::DownloadInnequinPlugin(FOnDownloadInnequinLog InLogCallback)
+{
+	UDownloadInnequinPluginAction* BlueprintNode = NewObject<UDownloadInnequinPluginAction>();
+	BlueprintNode->LogCallback = InLogCallback;
+	return BlueprintNode;
+}
+
+void UDownloadInnequinPluginAction::Activate()
+{
+	AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [this]()
+		{
+			bool bSuccess = false;
+			if (auto* World = GEditor->GetEditorWorldContext().World())
+			{
+				auto* InworldEditorApi = World->GetSubsystem<UInworldEditorApiSubsystem>();
+
+				const FString TempPath = FDesktopPlatformModule::Get()->GetUserTempPath();
+				const FString ZipLocation = FString::Format(TEXT("{0}{1}"), { TempPath, TEXT("InworldInnequin.zip") });
+				const FString ZipURL = TEXT("https://storage.googleapis.com/assets-inworld-ai/models/innequin/unreal/InworldInnequin_1.0.0.zip");
+				FStringFormatOrderedArguments GetZipFormattedArgs;
+				GetZipFormattedArgs.Add(FStringFormatArg(TEXT("--output")));
+				GetZipFormattedArgs.Add(FStringFormatArg(ZipLocation));
+				GetZipFormattedArgs.Add(FStringFormatArg(ZipURL));
+				const FString GetZipCommandLineArgs = FString::Format(TEXT("{0} \"{1}\" \"{2}\""), GetZipFormattedArgs);
+				TSharedPtr<FMonitoredProcess> GetZipProcess = MakeShareable(new FMonitoredProcess(TEXT("curl"), GetZipCommandLineArgs, true));
+				NotifyLog(TEXT("Downloading..."));
+				GetZipProcess->OnOutput().BindLambda([this](const FString& Output)
+					{
+						NotifyLog(Output);
+					}
+				);
+				if (!GetZipProcess->Launch())
+				{
+					NotifyComplete(false);
+					return;
+				}
+				while (GetZipProcess->Update())
+				{
+					FPlatformProcess::Sleep(0.01f);
+				}
+				const int GetZipRetCode = GetZipProcess->GetReturnCode();
+				if (GetZipRetCode != 0)
+				{
+					NotifyComplete(false);
+					return;
+				}
+				NotifyLog(TEXT("Downloading Complete!"));
+
+				const FString PluginLocation = FPaths::ProjectPluginsDir();
+				FStringFormatOrderedArguments UnzipFormattedArgs;
+				UnzipFormattedArgs.Add(FStringFormatArg(TEXT("-xf")));
+				UnzipFormattedArgs.Add(FStringFormatArg(ZipLocation));
+				UnzipFormattedArgs.Add(FStringFormatArg(TEXT("-C")));
+				UnzipFormattedArgs.Add(FStringFormatArg(PluginLocation));
+				const FString UnzipCommandLineArgs = FString::Format(TEXT("{0} \"{1}\" {2} \"{3}\""), UnzipFormattedArgs);
+				TSharedPtr<FMonitoredProcess> UnzipProcess = MakeShareable(new FMonitoredProcess(TEXT("tar"), UnzipCommandLineArgs, true));
+				NotifyLog(TEXT("Extracting..."));
+				if (!UnzipProcess->Launch())
+				{
+					NotifyComplete(false);
+					return;
+				}
+				while (UnzipProcess->Update())
+				{
+					FPlatformProcess::Sleep(0.01f);
+				}
+				const int UnzipRetCode = UnzipProcess->GetReturnCode();
+				if (UnzipRetCode != 0)
+				{
+					NotifyComplete(false);
+					return;
+				}
+				NotifyLog(TEXT("Extracting Complete!"));
+
+				NotifyComplete(true);
+			}
+		}
+	);
+}
+
+void UDownloadInnequinPluginAction::NotifyLog(const FString& Message)
+{
+	if (IsInGameThread())
+	{
+		LogCallback.ExecuteIfBound(Message);
+	}
+	else
+	{
+		AsyncTask(ENamedThreads::GameThread, [this, Message]()
+			{
+				NotifyLog(Message);
+			}
+		);
+	}
+}
+
+void UDownloadInnequinPluginAction::NotifyComplete(bool bSuccess)
+{
+	if (IsInGameThread())
+	{
+		if (bSuccess)
+		{
+			if (auto* World = GEditor->GetEditorWorldContext().World())
+			{
+				auto* InworldEditorApi = World->GetSubsystem<UInworldEditorApiSubsystem>();
+				InworldEditorApi->NotifyRestartRequired();
+			}
+			NotifyLog(TEXT("Restart Required!"));
+		}
+		else
+		{
+			NotifyLog(TEXT("Something went wrong! Please download file and install manually."));
+		}
+		DownloadComplete.Broadcast(bSuccess);
+	}
+	else
+	{
+		AsyncTask(ENamedThreads::GameThread, [this, bSuccess]()
+			{
+				NotifyComplete(bSuccess);
+			}
+		);
+	}
+}
