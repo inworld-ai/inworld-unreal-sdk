7b1e11575eda8955ca1ecdd8f9620769ca8fe05c
Artem Vozniuk <a_vozniuk@inworld.ai>
2023-08-30 14:02:12 +0200

    fix mac/ios build logs

---
+++ build-scripts/ios-build-game.sh
@@ -0,0 +1,1 @@
+sh "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildGame -project="$PWD/TestInworldFPS/TestInworldFPS/TestInworldFPS.uproject" -platform=IOS  -notools -configuration=Development+Shipping
