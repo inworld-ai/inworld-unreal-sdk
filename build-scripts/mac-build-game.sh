3a4a3ecdf7d9918b926ae10d1d311f247befd810
art-vozniuk <a_vozniuk@inworld.ai>
2023-08-30 15:11:33 +0200

    fix github actions

--- build-scripts/mac-build-game.sh
+++ build-scripts/mac-build-game.sh
@@ -1,1 +1,2 @@
 sh "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildGame -project="$PWD/TestInworldFPS/TestInworldFPS/TestInworldFPS.uproject" -platform=Mac  -notools -configuration=Development+Shipping
+echo build-finished
