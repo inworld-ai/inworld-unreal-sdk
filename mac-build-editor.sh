17901a688b4506b109f75d5fac842d82ecf00810
art-vozniuk <a_vozniuk@inworld.ai>
2023-08-30 14:52:52 +0200

    github actions

--- mac-build-editor.sh
+++ mac-build-editor.sh
@@ -1,2 +1,3 @@
-#sh "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildEditor -project="$PWD/TestInworldFPS/TestInworldFPS/TestInworldFPS.uproject" -platform=Mac  -notools -configuration=Development+Shipping
 echo hello
+sh "/Users/Shared/Epic Games/UE_5.1/Engine/Build/BatchFiles/RunUAT.sh" BuildEditor -project="$PWD/TestInworldFPS/TestInworldFPS/TestInworldFPS.uproject" -platform=Mac  -notools -configuration=Development+Shipping
+echo hello2
