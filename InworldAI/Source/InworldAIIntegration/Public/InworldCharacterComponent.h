8e32113c37ed1a81ad1a3cbcc20e0b1905697269
Matt Carey <MattCarey@gmx.com>
2023-11-01 16:41:34 -0400

    add blueprintpure=false to sendtrigger
    send trigger is being treated as blueprint pure, force execution pin

--- InworldAI/Source/InworldAIIntegration/Public/InworldCharacterComponent.h
+++ InworldAI/Source/InworldAIIntegration/Public/InworldCharacterComponent.h
@@ -124,7 +124,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Interaction")
 	void SendTextMessage(const FString& Text) const;
 
-	UFUNCTION(BlueprintCallable, Category = "Interaction", meta = (AutoCreateRefTerm = "Params"))
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, Category = "Interaction", meta = (AutoCreateRefTerm = "Params"))
 	void SendTrigger(const FString& Name, const TMap<FString, FString>& Params) const;
 	[[deprecated("UInworldCharacterComponent::SendCustomEvent is deprecated, please use UInworldCharacterComponent::SendTrigger")]]
 	void SendCustomEvent(const FString& Name) const { SendTrigger(Name, {}); }
