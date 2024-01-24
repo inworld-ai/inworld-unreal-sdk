974af3d5078d217c0ba5eb11eec5571c7c2cc837
Matt Carey <MattCarey@gmx.com>
2024-01-24 11:26:36 -0500

    Update Copyright claim and Inworld SDK License
    Update Copyright claim and Inworld SDK License
    https://inworldai.atlassian.net/browse/INTG-902

--- InworldMetahuman/Source/InworldMetahumanEditor/InworldMetahumanEditor.Build.cs
+++ InworldMetahuman/Source/InworldMetahumanEditor/InworldMetahumanEditor.Build.cs
@@ -1,4 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+/**
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ */
 
 using System.IO;
 using UnrealBuildTool;
