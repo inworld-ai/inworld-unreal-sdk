8ba12b04899b00fac8606947940378ae83c44181
Artem Vozniuk <a_vozniuk@inworld.ai>
2024-04-10 12:24:17 +0200

    use static libs on ios

--- InworldAI/Source/ThirdParty/InworldAINDKLibrary/InworldAINDKLibrary.Build.cs
+++ InworldAI/Source/ThirdParty/InworldAINDKLibrary/InworldAINDKLibrary.Build.cs
@@ -69,7 +69,7 @@ public class InworldAINDKLibrary : ModuleRules
         PublicDefinitions.Add("INWORLD_LOG=1");
         PublicDefinitions.Add("INWORLD_LOG_CALLBACK=1");
 
-        const bool bUseSharedInworldNDK = true;
+        bool bUseSharedInworldNDK = Target.Platform != UnrealTargetPlatform.IOS;
         if (bUseSharedInworldNDK)
         {
             PublicDefinitions.Add("INWORLD_NDK_SHARED=1");
@@ -160,6 +160,7 @@ public class InworldAINDKLibrary : ModuleRules
         {
             PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyLibrariesDirectory, "libinworld-ndk.dylib"));
             RuntimeDependencies.Add(Path.Combine(ThirdPartyLibrariesDirectory, "libinworld-ndk.dylib"));
+            PublicDelayLoadDLLs.Add(Path.Combine(ThirdPartyLibrariesDirectory, "libinworld-ndk.dylib"));
         }
         else if(Target.Platform == UnrealTargetPlatform.Android && bUseSharedInworldNDK)
         {
