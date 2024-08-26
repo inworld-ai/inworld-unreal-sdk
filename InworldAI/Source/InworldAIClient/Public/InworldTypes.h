7844caba36651d10165f107b716c7d4a3d8d39e4
Matt Carey <MattCarey@gmx.com>
2024-08-26 11:17:10 -0400

    Update InworldTypes.h

--- InworldAI/Source/InworldAIClient/Public/InworldTypes.h
+++ InworldAI/Source/InworldAIClient/Public/InworldTypes.h
@@ -85,49 +85,13 @@ struct FInworldAuth
 	GENERATED_BODY()
 
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Capability")
-    FString Base64Signature;
+    FString Base64Signature = "";
 
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Capability")
-    FString ApiKey;
+    FString ApiKey = "";
 
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Capability")
-    FString ApiSecret;
-};
-
-USTRUCT(BlueprintType)
-struct FTestRedir
-{
-    GENERATED_BODY()
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Name;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Key;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Secret;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    bool IsActive = false;
-};
-
-USTRUCT(BlueprintType)
-struct FInworldStudioUserApiKeyData
-{
-    GENERATED_BODY()
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Name;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Key;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    FString Secret;
-
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Studio")
-    bool IsActive = false;
+    FString ApiSecret = "";
 };
 
 USTRUCT(BlueprintType)
