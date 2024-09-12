00f190ab2e05df516cf9247dec95ac3fc2d491a2
Matt Carey <mcarey@inworld.ai>
2024-09-12 10:57:29 -0400

    Studio api (#157)
    studio api

---
+++ InworldAI/Source/InworldAIIntegration/Public/InworldAIIntegrationSettings.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2022-2024 Theai, Inc. dba Inworld AI
+ *
+ * Use of this source code is governed by the Inworld.ai Software Development Kit License Agreement
+ * that can be found in the LICENSE.md file or at https://www.inworld.ai/sdk-license
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "InworldAIIntegrationSettings.generated.h"
+
+UCLASS(config=InworldAI)
+class INWORLDAIINTEGRATION_API UInworldAIIntegrationSettings : public UObject
+{
+	GENERATED_BODY()
+public:
+	UInworldAIIntegrationSettings(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(config, EditAnywhere, Category = "Inworld")
+	FString StudioApiKey;
+};
